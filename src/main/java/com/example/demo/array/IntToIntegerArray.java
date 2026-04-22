799efb61b4091cdb2ae9e719dbe911b38ec7be53
StoryOnJava <jagzapsagar228@gmail.com>
2026-04-22 19:05:26 +0530

    Interview code added

--- src/main/java/com/example/demo/array/IntToIntegerArray.java
+++ src/main/java/com/example/demo/array/IntToIntegerArray.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 public class IntToIntegerArray {
 	public static void main(String[] args) {
 		int[] ar = {4,9,3,7};
-		
 		Integer[] array = Arrays.stream(ar).boxed().toArray(Integer[]::new);
 	}
 
--- src/main/java/com/example/demo/array/SortArray.java
+++ src/main/java/com/example/demo/array/SortArray.java
@@ -1,5 +1,8 @@
 package com.example.demo.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SortArray {
 	public static void main(String[] args) {
 		int[] ar = {2,6,3,1,9};
@@ -31,6 +34,34 @@ public class SortArray {
 		for(int t:ar) {
 			System.out.println(t);
 		}
+		
+		//Reverse String
+		System.out.println("Revrse String");
+		String str = "abcd";
+		
+		String reduce = str.chars().mapToObj(e -> (char)e).map(t -> String.valueOf(t)).reduce(" ",(a,b) -> b + a);
+		System.out.println(reduce);
+		
+		char[] sarr = str.toCharArray();
+		int lenn = sarr.length;
+		for(int h=0;h<lenn/2;h++) {
+			
+			int t = ar[h];
+			ar[h] = ar[lenn-1-h];
+			ar[lenn-1-h] = t;
+		}
+		for(char t:sarr) {
+			System.out.println(""+t);
+		}
+		System.out.println("---------------");
+		List<String> list = List.of("abc", "xyz");
+		
+		List<Character> list3 = list.stream().flatMap(e -> e.chars().mapToObj(t -> (char)t)).toList();
+		System.out.println(list3);
+		List<Integer> list2 = list.stream().map(e -> e.length()).toList();
+		System.out.println(list2);
+		
+		
 	}
 
 }
